eb4b99b0f57b173e3d23b976f1ad6474f203e74a
Jeph Bayfield <jeph_b@hotmail.com>
2022-01-19 04:04:22 +0000

    Added numeric notnullconditional extensions

--- src/JustDeclare.App/Extensions/NumericNotNullExtensions.cs
+++ src/JustDeclare.App/Extensions/NumericNotNullExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using JustDeclare.Conditions;
-using JustDeclare.Main.ValidationChecks;
 using JustDeclare.Models;
 using System;
 
@@ -7,32 +6,76 @@ namespace JustDeclare
 {
     public static partial class JustDeclareExtensions
     {
+        public static ValidationCheck MustEqual<T>(this NotNullCondition<T?> conditional, T target)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustEqual(target));
+        }
+
+        public static ValidationCheck MustNotEqual<T>(this NotNullCondition<T?> conditional, T target)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustNotEqual(target));
+        }
+
         public static ValidationCheck MustBeZero<T>(this NotNullCondition<T?> conditional)
             where T : struct, IComparable, IFormattable
         {
-            return conditional.CreateValidationCheck(x => new NumericEqual<T>(x, 0));
+            return conditional.CreateValidationCheck(MustBeZero);
         }
 
-        public static ValidationCheck MustBeNoMoreThan<T>(this NotNullCondition<T?> conditional, IComparable maximum)
+        public static ValidationCheck MustNotBeZero<T>(this NotNullCondition<T?> conditional)
             where T : struct, IComparable, IFormattable
         {
-            return conditional.CreateValidationCheck(x => new NumericMaximum<T>(x, maximum));
+            return conditional.CreateValidationCheck(MustNotBeZero);
         }
 
-        public static ValidationCheck MustBeNoLessThan<T>(this NotNullCondition<T?> conditional, IComparable minimum)
+        public static ValidationCheck MustBeNoMoreThan<T>(this NotNullCondition<T?> conditional, T maximum)
             where T : struct, IComparable, IFormattable
         {
-            return conditional.CreateValidationCheck(x => new NumericMinimum<T>(x, minimum));
+            return conditional.CreateValidationCheck(x => x.MustBeNoMoreThan(maximum));
         }
 
-        public static ValidationCheck MustBeNoLongerThan(this NotNullCondition<string> conditional, int maximumLength)
+        public static ValidationCheck MustBeNoLessThan<T>(this NotNullCondition<T?> conditional, T minimum)
+            where T : struct, IComparable, IFormattable
         {
-            return conditional.CreateValidationCheck(x => x.MustBeNoLongerThan(maximumLength));
+            return conditional.CreateValidationCheck(x => x.MustBeNoLessThan(minimum));
         }
 
-        public static ValidationCheck MustBeNoShorterThan(this NotNullCondition<string> conditional, int minimumLength)
+        public static ValidationCheck MustBeGreaterThan<T>(this NotNullCondition<T?> conditional, T target)
+            where T : struct, IComparable, IFormattable
         {
-            return conditional.CreateValidationCheck(x => x.MustBeNoShorterThan(minimumLength));
+            return conditional.CreateValidationCheck(x => x.MustBeGreaterThan(target));
+        }
+
+        public static ValidationCheck MustBeLessThan<T>(this NotNullCondition<T?> conditional, T target)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeLessThan(target));
+        }
+
+        public static ValidationCheck MustBeGreaterThanOrEqualTo<T>(this NotNullCondition<T?> conditional, T minimum)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeGreaterThanOrEqualTo(minimum));
+        }
+
+        public static ValidationCheck MustBeLessThanOrEqualTo<T>(this NotNullCondition<T?> conditional, T maximum)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeLessThanOrEqualTo(maximum));
+        }
+
+        public static ValidationCheck MustBeInRange<T>(this NotNullCondition<T?> conditional, T rangeStart, T rangeEnd)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeInRange(rangeStart, rangeEnd));
+        }
+
+        public static ValidationCheck MustNotBeInRange<T>(this NotNullCondition<T?> conditional, T rangeStart, T rangeEnd)
+            where T : struct, IComparable, IFormattable
+        {
+            return conditional.CreateValidationCheck(x => x.MustNotBeInRange(rangeStart, rangeEnd));
         }
     }
 }
--- src/JustDeclare.App/Extensions/StringNotNullExtensions.cs
+++ src/JustDeclare.App/Extensions/StringNotNullExtensions.cs
@@ -15,6 +15,16 @@ namespace JustDeclare
             return conditional.CreateValidationCheck(MustBeBlank);
         }
 
+        public static ValidationCheck MustBeNoLongerThan(this NotNullCondition<string> conditional, int maximumLength)
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeNoLongerThan(maximumLength));
+        }
+
+        public static ValidationCheck MustBeNoShorterThan(this NotNullCondition<string> conditional, int minimumLength)
+        {
+            return conditional.CreateValidationCheck(x => x.MustBeNoShorterThan(minimumLength));
+        }
+
         public static ValidationCheck MustMatchPattern(this NotNullCondition<string> conditional, string pattern)
         {
             return conditional.CreateValidationCheck(x => x.MustMatchPattern(pattern));

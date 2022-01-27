481a8dc239389bda1a32b7dbf6b28ed9823fc16f
Jeph Bayfield <jeph_b@hotmail.com>
2022-01-27 00:40:28 +0000

    started pattern matching tests

---
+++ src/dotValidate.Tests/IsolatedRuleTests/StringBased/MustMatchPattern.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace dotValidate.Tests.IsolatedRuleTests.StringBased
+{
+    public class MustMatchPattern : StringTestClass
+    {
+        private const string NUMBERIC_PATTERM = "^[0-9]+$";
+        private const string EMAIL_PATTERN = "/^S+@S+.S+$/";
+
+        private class TestClassValidationRules : ValidationRules<TestClass>
+        {
+            public TestClassValidationRules()
+            {
+                DeclareRules(
+                        x => x.TestNullable.MustMatchPattern(NUMBERIC_PATTERM),
+                        x => x.TestNonNullable.MustMatchPattern(EMAIL_PATTERN)
+                    );
+            }
+        }
+
+        [Fact]
+        public void GivenAboveRules_WhenAllValuesMatchPatterns_PassTest()
+        {
+            // Arrange
+            var request = GetTestClass();
+            var validator = new TestClassValidationRules();
+
+            // Act
+            var result = validator.Validate(request);
+
+            // Assert
+            result.HasFailures.ShouldBeFalse();
+        }
+
+        private static TestClass GetTestClass()
+        {
+            return new TestClass()
+            {
+                TestNullable = "012345",
+                TestNonNullable = "abc@def.com"
+            };
+        }
+    }
+}
---
+++ src/dotValidate.Tests/IsolatedRuleTests/StringBased/MustNotMatchPattern.cs
@@ -0,0 +1,28 @@
+﻿namespace dotValidate.Tests.IsolatedRuleTests.StringBased
+{
+    public class MustNotMatchPattern : StringTestClass
+    {
+        private const string NUMBERIC_PATTERM = "^[0-9]+$";
+        private const string EMAIL_PATTERN = "/^S+@S+.S+$/";
+
+        private class TestClassValidationRules : ValidationRules<TestClass>
+        {
+            public TestClassValidationRules()
+            {
+                DeclareRules(
+                        x => x.TestNullable.MustNotMatchPattern(NUMBERIC_PATTERM),
+                        x => x.TestNonNullable.MustNotMatchPattern(EMAIL_PATTERN)
+                    );
+            }
+        }
+
+        private static TestClass GetTestClass()
+        {
+            return new TestClass()
+            {
+                TestNullable = "ABCDEF",
+                TestNonNullable = "abcdef.com"
+            };
+        }
+    }
+}

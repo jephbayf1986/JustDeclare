d2ab68d650f6f8ccf2fb339f63a4f11392b0f480
Jeph Bayfield <jeph_b@hotmail.com>
2022-02-06 00:56:55 +0000

    Added base comments

--- src/dotValidate.App/Base/IValidator.cs
+++ src/dotValidate.App/Base/IValidator.cs
@@ -1,10 +1,21 @@
-﻿using dotValidate;
-using dotValidate.Models;
+﻿using dotValidate.Models;
 
 namespace dotValidate
 {
+    /// <summary>
+    /// <b>IValidator</b><br/>
+    /// Interface for validating requests using <i>dotValidate</i>. 
+    /// </summary>
     public interface IValidator
     {
+        /// <summary>
+        /// <b>Validate Request</b><br/>
+        /// Runs each validation rule against the request and returns a result based on the outcome.
+        /// </summary>
+        /// <typeparam name="TValidator">Validation Rules for Request</typeparam>
+        /// <typeparam name="TRequest">Request to be Validated</typeparam>
+        /// <param name="request">Request to be Validated</param>
+        /// <returns>Validation Result</returns>
         ValidationResult ValidateRequest<TValidator, TRequest>(TRequest request)
             where TValidator : ValidationRules<TRequest>, new();
     }
--- src/dotValidate.App/Base/ValidationRules.cs
+++ src/dotValidate.App/Base/ValidationRules.cs
@@ -7,15 +7,40 @@ using System.Linq.Expressions;
 
 namespace dotValidate
 {
+    /// <summary>
+    /// <b>Validation Rules</b><br/>
+    /// Inherit from this <i>dotValidate</i> class to declare the set of rules which a request must obey in order to pass validation
+    /// </summary>
+    /// <typeparam name="TRequest">Request Type</typeparam>
     public abstract class ValidationRules<TRequest>
     {
         private IEnumerable<ValidationTestContainer<TRequest>> _validationTestFuncs;
 
+        /// <summary>
+        /// <b>Declare Rules</b><br/>
+        /// Call this from the constructor and pass a list of expressions to create the validation rules.
+        /// <para>
+        /// <i>Example:</i> <br />
+        /// <code> 
+        /// DeclareRules (
+        /// x => x.Id.MustNotBeNull(),
+        /// x => x.Name.MustNotBeBlank()
+        /// )
+        /// </code>
+        /// </para>
+        /// </summary>
+        /// <param name="rules"></param>
         protected void DeclareRules(params Expression<Func<TRequest, ValidationCheck>>[] rules)
         {
             _validationTestFuncs = rules.CreateValidationTestFuncs();
         }
 
+        /// <summary>
+        /// <b>Validate</b><br/>
+        /// Runs each validation rule against the request and returns a result based on the outcome.
+        /// </summary>
+        /// <param name="request">Request to be Validated</param>
+        /// <returns>Validation Result</returns>
         public ValidationResult Validate(TRequest request)
         {
             return _validationTestFuncs.RunAllTestsFor(request);

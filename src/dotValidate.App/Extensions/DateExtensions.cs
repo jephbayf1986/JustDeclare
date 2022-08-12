ff9e983346e66349b7c42b033b2a4d380e86fd05
Jeph Bayfield <jeph_b@hotmail.com>
2022-08-12 22:24:45 +0100

    Added documentation for datemax an datemin - untested - not working

--- src/dotValidate.App/Extensions/DateExtensions.cs
+++ src/dotValidate.App/Extensions/DateExtensions.cs
@@ -56,78 +56,247 @@ namespace dotValidate
                         .Inverted();
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private static ValidationCheck MustBeNoLaterThan(this DateTime value, DateTime maximumDate)
+        /// <summary>
+        /// Must Be Later Than Or Equal To...
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="minimumDateTime">Minimum allowed DateTime</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThanOrEqualTo(this DateTime value, DateTime minimumDateTime)
         {
-            return new DateMaximum(value, maximumDate);
+            return new DateMinimum(value, minimumDateTime);
         }
 
-        private static ValidationCheck MustBeNoEarlierThan(this DateTime value, DateTime minimumDate)
+        /// <summary>
+        /// Must Be Later Than Or Equal To...
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <param name="minimumDateTime">Minimum allowed DateTime</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThanOrEqualTo(this DateTime? value, DateTime minimumDateTime)
         {
-            return new DateMinimum(value, minimumDate);
+            return new DateMinimum(value, minimumDateTime);
         }
 
-        private static ValidationCheck MustBeLaterThan(this DateTime value, DateTime maximumDate)
+        /// <summary>
+        /// Must Be Earlier Than Or Equal To...
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="maximumDateTime">Maximum allowed DateTime</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThanOrEqualTo(this DateTime value, DateTime maximumDateTime)
         {
-            return new DateMaximum(value, maximumDate);
+            return new DateMaximum(value, maximumDateTime);
         }
 
-        private static ValidationCheck MustBeEarlierThan(this DateTime value, DateTime minimumDate)
+        /// <summary>
+        /// Must Be Earlier Than Or Equal To...
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <param name="maximumDateTime">Maximum allowed DateTime</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThanOrEqualTo(this DateTime? value, DateTime maximumDateTime)
         {
-            return new DateMinimum(value, minimumDate);
+            return new DateMaximum(value, maximumDateTime);
         }
 
-        private static ValidationCheck MustBeNoYoungerThan(this DateTime value, int minimumYears)
+        /// <summary>
+        /// Must Be Later Than...
+        /// </summary>
+        /// <param name="value">DateTime subject to validation rule</param>
+        /// <param name="targetDateTime">Target DateTime to be later than</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThan(this DateTime value, DateTime targetDateTime)
+        {
+            return new DateMaximum(value, targetDateTime)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be Later Than...
+        /// </summary>
+        /// <param name="value">Nullable DateTime subject to validation rule</param>
+        /// <param name="targetDateTime">Target DateTime to be later than</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThan(this DateTime? value, DateTime targetDateTime)
+        {
+            return new DateMaximum(value, targetDateTime)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be Earlier Than...
+        /// </summary>
+        /// <param name="value">DateTime subject to validation rule</param>
+        /// <param name="targetDateTime">Target DateTime to be earlier than</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThan(this DateTime value, DateTime targetDateTime)
+        {
+            return new DateMinimum(value, targetDateTime)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be Earlier Than...
+        /// </summary>
+        /// <param name="value">Nullable DateTime subject to validation rule</param>
+        /// <param name="targetDateTime">Target DateTime to be earlier than</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThan(this DateTime? value, DateTime targetDateTime)
+        {
+            return new DateMinimum(value, targetDateTime)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be Later Than Or The Same Date As...
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="minimumDate">Minimum allowed Date (Note: Time Part is ignored)</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThanOrSameDateAs(this DateTime value, DateTime minimumDate)
+        {
+            return new DateMinimum(value, minimumDate.Date);
+        }
+
+        /// <summary>
+        /// Must Be Later Than Or The Same Date As...
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <param name="minimumDate">Minimum allowed Date (Note: Time Part is ignored)</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeLaterThanOrSameDateAs(this DateTime? value, DateTime minimumDate)
+        {
+            return new DateMinimum(value, minimumDate.Date);
+        }
+
+        /// <summary>
+        /// Must Be Earlier Than Or The Same Date As...
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="maximumDate">Maximum allowed Date (Note: Time Part is ignored)</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThanOrSameDateAs(this DateTime value, DateTime maximumDate)
+        {
+            var inverseMinDate = maximumDate.Date.AddDays(1);
+
+            return new DateMinimum(value, inverseMinDate)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be Earlier Than Or The Same Date As...
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <param name="maximumDate">Maximum allowed Date (Note: Time Part is ignored)</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeEarlierThanOrSameDateAs(this DateTime? value, DateTime maximumDate)
+        {
+            var inverseMinDate = maximumDate.Date.AddDays(1);
+
+            return new DateMinimum(value, inverseMinDate)
+                        .Inverted();
+        }
+
+        /// <summary>
+        /// Must Be In Past
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeInPast(this DateTime value)
+        {
+            return value.MustBeEarlierThan(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Must Be In Past
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeInPast(this DateTime? value)
+        {
+            return value.MustBeEarlierThan(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Must Be In Future
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeInFuture(this DateTime value)
+        {
+            return value.MustBeLaterThan(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Must Be In Future
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck MustBeInFuture(this DateTime? value)
+        {
+            return value.MustBeLaterThan(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Age Must Be Younger Than
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="maximumYears">Maximum Allowed Age in Years</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck AgeMustBeYoungerThan(this DateTime value, int maximumYears)
+        {
+            var targetDateTime = DateTime.Now.Date.AddYears(-maximumYears - 1);
+
+            return value.MustBeLaterThan(targetDateTime);
+        }
+
+        /// <summary>
+        /// Age Must Be Younger Than
+        /// </summary>
+        /// <param name="value">Nullable DateTime value to validate</param>
+        /// <param name="maximumYears">Maximum Allowed Age in Years</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck AgeMustBeYoungerThan(this DateTime? value, int maximumYears)
+        {
+            var targetDateTime = DateTime.Now.Date.AddYears(-maximumYears - 1);
+
+            return value.MustBeLaterThan(targetDateTime);
+        }
+
+        /// <summary>
+        /// Age Must Be Older Than
+        /// </summary>
+        /// <param name="value">DateTime value to validate</param>
+        /// <param name="minimumYears">Minimum Allowed Age in Years</param>
+        /// <returns>Validation Check - <i>dotValidate</i> container for all input and output information for the validation rule</returns>
+        public static ValidationCheck AgeMustBeOlderThan(this DateTime value, int minimumYears)
         {
             var maximumDate = DateTime.Now.Date.AddYears(-minimumYears);
 
-            return value.MustBeNoLaterThan(maximumDate);
+            return value.MustBeEarlierThanOrSameDateAs(maximumDate);
         }
 
-        private static ValidationCheck MustBeNoOlderThan(this DateTime value, int maximumYears)
-        {
-            var minimumDate = DateTime.Now.Date.AddYears(-maximumYears);
 
-            return value.MustBeNoEarlierThan(minimumDate);
-        }
 
-        private static ValidationCheck MustBeInPast(this DateTime value)
-        {
-            return value.MustBeNoLaterThan(DateTime.Now);
-        }
 
-        private static ValidationCheck MustBeInFuture(this DateTime value)
-        {
-            return value.MustBeNoEarlierThan(DateTime.Now);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private static ValidationCheck MustBeInRange(this DateTime value, DateTime rangeStart, DateTime rangeEnd)
         {
@@ -139,18 +308,18 @@ namespace dotValidate
             return value.MustBeInRange(rangeStart, rangeEnd)
                         .Inverted();
         }
-
-        private static ValidationCheck MustBeInAgeRange(this DateTime value, int rangeStartYears, int rangeEndYears)
+        
+        private static ValidationCheck AgeMustBeInRange(this DateTime value, int rangeStartYears, int rangeEndYears)
         {
             var rangeStart = DateTime.Now.Date.AddYears(-rangeStartYears);
             var rangeEnd = DateTime.Now.Date.AddYears(-rangeEndYears);
 
             return new DateRange(value, rangeStart, rangeEnd);
         }
-
-        private static ValidationCheck MustNotBeInAgeRange(this DateTime value, int rangeStartYears, int rangeEndYears)
+        
+        private static ValidationCheck AgeMustNotBeInRange(this DateTime value, int rangeStartYears, int rangeEndYears)
         {
-            return value.MustBeInAgeRange(rangeStartYears, rangeEndYears)
+            return value.AgeMustBeInRange(rangeStartYears, rangeEndYears)
                         .Inverted();
         }
 
@@ -214,7 +383,7 @@ namespace dotValidate
             return new DateMonthRange(value, startRange, endRange);
         }
 
-        public static ValidationCheck MonthMustBeNotInRange(this DateTime value, MonthOfYear startRange, MonthOfYear endRange)
+        private static ValidationCheck MonthMustBeNotInRange(this DateTime value, MonthOfYear startRange, MonthOfYear endRange)
         {
             return value.MonthMustBeInRange(startRange, endRange)
                         .Inverted();
